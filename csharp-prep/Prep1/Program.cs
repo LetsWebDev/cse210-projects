ec1de8e2fc56f0ee7dca455f265c711efc643f61
LetsWebDev <david079kv@byui.edu>
2026-04-28 16:33:38 -0600

    Updated Prep1 and Prep2 programs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string dw_firstname = Dw_Question("What is your First Name?");
+        string dw_lastname = Dw_Question("What is your Last Name?");
+
+        Console.WriteLine($"\nYour name is {dw_lastname}.");
+        Console.WriteLine($"{dw_firstname} {dw_lastname}");
+    }
+
+    static string Dw_Question(string text)
+    {
+        Console.WriteLine(text);
+        string dw_answer = Console.ReadLine();
+        return(dw_answer);
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,68 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        bool DW_is_grading = true;
+        while (DW_is_grading){
+        Console.WriteLine("\n\nWhat is your Grade percentage?");
+        int DW_per = int.Parse(Console.ReadLine());
+        string DW_grade;
+        if (DW_per >= 90)
+        {
+            DW_grade = "A";
+        }
+        else if (DW_per >= 80)
+        {
+            DW_grade = "B";
+        }
+        else if (DW_per >= 70)
+        {
+            DW_grade = "C";
+        }
+        else if (DW_per >= 60)
+        {
+            DW_grade = "D";
+        }
+        else
+        {
+            DW_grade = "F";
+        }
+
+        int DW_dec = DW_per%10;
+        if (DW_grade != "F")
+            {
+                if (DW_per >= 100)
+                    {
+                        DW_grade = $"{DW_grade}+";
+                    }
+                else if (DW_dec < 3)
+                    {
+                        DW_grade = $"{DW_grade}-";
+                    }
+                else if (DW_dec > 7)
+                    {
+                        
+                        DW_grade = $"{DW_grade}+";
+                    }
+            }
+            
+        Console.WriteLine(DW_grade);
+        Console.WriteLine($"Your grade is a {DW_grade}");
+        if (DW_per >= 70)
+            {
+                Console.WriteLine($"Congraduations! {DW_grade} is high enough to pass. You can move on to the next class.");
+            }
+        else
+            {
+                Console.WriteLine($"Sorry, {DW_grade} is not high enough to pass. Retake the class.");
+            }
+        
+        Console.WriteLine("\nDo you want to be graded again?");
+        string DW_answer = Console.ReadLine();
+        if ("no" == DW_answer)
+        {
+            DW_is_grading = false;
+        }
+        }
     }
 }
